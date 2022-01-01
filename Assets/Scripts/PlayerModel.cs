55240df07112e5e54f9ed370ff3f7b20b0c91947
Tyrease Teer <kayaneseera@gmail.com>
2022-01-01 06:58:41 -0700

    PlayerModel is Disabled
    No one can change the values directly except through code.

--- Assets/Scripts/PlayerModel.cs
+++ Assets/Scripts/PlayerModel.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using UnityEditor;
 [CreateAssetMenu(fileName = "New Player Model", menuName = "Player Model")]
 public class PlayerModel : ScriptableObject
 {
@@ -34,5 +34,68 @@ public class PlayerModel : ScriptableObject
 
     public void SetTotalModePlayed(int value) => TotalModesPlayed = value;
     public bool HasRecoverableAccount  = false;
+
+}
+
+[CustomEditor(typeof(PlayerModel))]
+public class PlayerModeScriptableObjectInspector : Editor
+{
+    SerializedProperty
+        _firstTimeUser,
+        _userName,
+        _uniqueIdentifier,
+        _blooxCurrency,
+        _playerAvatar,
+        _playerTheme,
+        _playerLevel,
+        _playerEXUntilNextLevel,
+        _timesPlayedSolo,
+        _timesPlayedWipeOut,
+        _timesPlayedSurvival,
+        _totalModesPlayed,
+        _hasRecoverableAccount;
+
+    public void OnEnable()
+    {
+        _firstTimeUser = serializedObject.FindProperty("FirstTimeUser");
+        _userName = serializedObject.FindProperty("UserName");
+        _uniqueIdentifier = serializedObject.FindProperty("UniqueIdentifier");
+        _blooxCurrency = serializedObject.FindProperty("BlooxCurrency");
+        _playerAvatar = serializedObject.FindProperty("PlayerAvatar");
+        _playerTheme = serializedObject.FindProperty("PlayerTheme");
+        _playerLevel = serializedObject.FindProperty("PlayerLevel");
+        _playerEXUntilNextLevel = serializedObject.FindProperty("PlayerEXUntilNextLevel");
+        _timesPlayedSolo = serializedObject.FindProperty("TimesPlayedSolo");
+        _timesPlayedWipeOut = serializedObject.FindProperty("TimesPlayedWipeOut");
+        _timesPlayedSurvival = serializedObject.FindProperty("TimesPlayedSurvival");
+        _totalModesPlayed = serializedObject.FindProperty("TotalModesPlayed");
+        _hasRecoverableAccount = serializedObject.FindProperty("HasRecoverableAccount");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        Draw();
+    }
+
+    void Draw()
+    {
+        EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.PropertyField(_firstTimeUser);
+            EditorGUILayout.PropertyField(_userName);
+            EditorGUILayout.PropertyField(_uniqueIdentifier);
+            EditorGUILayout.PropertyField(_blooxCurrency);
+            EditorGUILayout.PropertyField(_playerAvatar);
+            EditorGUILayout.PropertyField(_playerTheme);
+            EditorGUILayout.PropertyField(_playerLevel);
+            EditorGUILayout.PropertyField(_playerEXUntilNextLevel);
+            EditorGUILayout.PropertyField(_timesPlayedSolo);
+            EditorGUILayout.PropertyField(_timesPlayedWipeOut);
+            EditorGUILayout.PropertyField(_timesPlayedSurvival);
+            EditorGUILayout.PropertyField(_totalModesPlayed);
+            EditorGUILayout.PropertyField(_hasRecoverableAccount);
+        EditorGUI.EndDisabledGroup();
+
+        serializedObject.ApplyModifiedProperties();
+    }
 
 }
